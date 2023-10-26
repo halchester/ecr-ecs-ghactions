7010cae39b184867a5cd4ef593de4f00ba6765ca
Chester <chester.htooaunglinn@gmail.com>
2023-10-27 00:20:56 +0100

    feat: initialise vite app

---
+++ app/src/App.tsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello from EC2</h1>
+
+      <p className="footer">
+        <code>
+          Built with ❤️ by{" "}
+          <a href="https://www.linkedin.com/in/halchester/" target="_blank">
+            Chester
+          </a>{" "}
+          |{" "}
+          <a href="#" target="_blank">
+            Github
+          </a>{" "}
+          |{" "}
+          <a href="#" target="_blank">
+            See original post
+          </a>
+        </code>
+      </p>
+    </div>
+  );
+}
+
+export default App;
