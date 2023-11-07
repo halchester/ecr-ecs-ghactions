2dba8a12c1ee80e268c44285a7a205b520501b4c
Chester <chester.htooaunglinn@gmail.com>
2023-11-07 23:32:14 +0000

    feat: add a lifecycle policy for ecr images

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,30 @@
 
-module "blog_resources_repository" {
+module "vite_app_repository" {
   source  = "terraform-aws-modules/ecr/aws"
   version = "1.6.0"
 
-  repository_name                 = "blog-resources-repository"
+  repository_name                 = "vite-app-repository"
   repository_type                 = "private"
-  repository_image_tag_mutability = "MUTABLE"
-  create_lifecycle_policy         = false
+  repository_image_tag_mutability = "IMMUTABLE"
+  create_lifecycle_policy         = true
+
+  # only keep the latest 5 images
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire images by count"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 
   tags = merge(var.common_tags)
 }
